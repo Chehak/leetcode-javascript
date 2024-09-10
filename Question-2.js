efe51d728ce4149fcbe64a3d94bf001a858ec192
Chehak Gupta <gchehak18@gmail.com>
2024-09-10 22:47:24 +0530

    Update Question-2.js

--- Question-2.js
+++ Question-2.js
@@ -1,1 +1,18 @@
-Enter file contents here
+/* 
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed.
+Then return the number of elements in nums which are not equal to val.
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+*/
+
+var removeElement = function(nums, val) {
+    let k = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]== val){
+        nums.splice(i, 1);
+        i--;
+        }
+       k++
+    }
+};
+
+
